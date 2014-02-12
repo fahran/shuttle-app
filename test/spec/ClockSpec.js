7a72e669922344561c7d77620aa0b66f2c5292fd
fahran <fahran.wallace@gmail.com>
2014-02-12 20:55:42 +0000

    Switched to Jasmine 2. Added jasmine jquery for fixtures (bleh)

--- test/spec/ClockSpec.js
+++ test/spec/ClockSpec.js
@@ -1,13 +1,16 @@
-function setUpHTMLFixture() {
-    setFixtures('<h1 id="time">00:00:00</h1>');
-}
+describe("nextBus", function() {
+  
+  // it("contains spec with an expectation", function() {
+  //   setFixtures('<h1 id="time">00:00:00</h1>')
+  //   // spyOn(Date.prototype, 'getHours').andReturn(01)
+  //   // spyOn(Date.prototype, 'getMinutes').andReturn(02)
+  //   // spyOn(Date.prototype, 'getSeconds').andReturn(03)
+  //   // startClock();
+  //   // expect($("#time").text()).toBe("01:02:03")
+  // });
 
-describe("clock suite", function() {
-    beforeEach(function() {
-        setUpHTMLFixture();
-    });
-
-    it("contains spec with an expectation", function() {
-        expect(true).toBe(true);
-    });
+  it("returns the time of the next bus", function() {
+    nextBus();
+    expect($("#time").text()).toBe("01:02:03")
+  });
 });
--- www/js/clock.js
+++ www/js/clock.js
@@ -1,3 +1,9 @@
+var busTimes = ["10:00", "10.30", "10.40"]
+
+function nextBusTime() {
+
+}
+
 function startClock() {
     var currentdate = new Date();
     document.getElementById("time").innerHTML = currentdate;
