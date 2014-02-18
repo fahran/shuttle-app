2b364d8f121819a0ec45225a7384040e39eb0489
fahran <fahran.wallace@gmail.com>
2014-02-18 15:06:01 +0000

    Fixed 'red when bus is near' functionality

--- test/spec/appSpec.js
+++ test/spec/appSpec.js
@@ -13,7 +13,19 @@ describe("app", function() {
       expect($("#timeToNextBus").text()).toBe("00:10:00");
     });
 
-    it("app.findNextBuses makes the time red when the bus is 5 mins away", function() {
+    it("app.findNextBuses makes the time red when the bus is less than 5 mins away", function() {
+      setFixtures('<p id="timeToNextBus"><time></time></p>' +  
+                  '<p id="nextBusTime"><time></time></p>');
+
+      var busService = new BusTimeService("");
+
+      spyOn(busService, 'nextBusAfter').and.returnValue(new Time(1,20,0));
+
+      findNextBuses(busService, new Time(1,15,1));
+      expect($("#timeToNextBus")[0].classList.contains("red")).toBe(true);
+    });
+
+    it("app.findNextBuses does not make time red when bus is 5 mins or more away", function() {
       setFixtures('<p id="timeToNextBus"><time></time></p>' +  
                   '<p id="nextBusTime"><time></time></p>');
 
@@ -22,6 +34,6 @@ describe("app", function() {
       spyOn(busService, 'nextBusAfter').and.returnValue(new Time(1,20,0));
 
       findNextBuses(busService, new Time(1,15,0));
-      expect($("#timeToNextBus").classList).toContain("red");
+      expect($("#timeToNextBus")[0].classList.contains("red")).toBe(false);
     });
 });
